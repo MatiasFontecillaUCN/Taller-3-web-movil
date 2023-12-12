23568e0ca85a57d6407d2e48c2beef4ae26954da
MatiasFontecillaUCN <matias.fontecilla@alumnos.ucn.cl>
2023-12-12 10:35:50 -0300

    fix: fullname lenght

--- backend/MobileHub/src/dto/users/UpdateUserDto.cs
+++ backend/MobileHub/src/dto/users/UpdateUserDto.cs
@@ -13,10 +13,10 @@ namespace MobileHub.src.dto.users
         [UCNEmailAddressAtributte(errorMessage:"Correo invalido")]
         public string Email { get; set; } = string.Empty;
 
-        /// <summary>
-        /// Nombre del usuario.
-        /// </summary>
+        
         [Required]
+        [StringLength(150, ErrorMessage = "El nombre completo no puede tener mas de 150 caracteres")]
+        [MinLength(10, ErrorMessage = "El nombre completo no puede tener menos de 10 caracteres")]
         public string Fullname { get; set; } = null!;
 
         /// <summary>
