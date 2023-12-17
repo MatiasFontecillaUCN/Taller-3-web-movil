1a66f575b77b9ce3b5baf8f73e228cb4fd7857d7
MatiasFontecillaUCN <matias.fontecilla@alumnos.ucn.cl>
2023-12-17 14:12:26 -0300

    refactor: create component CustomAppBar

--- frontend/MobileHub/app/auth/_layout.tsx
+++ frontend/MobileHub/app/auth/_layout.tsx
@@ -1,21 +1,10 @@
 import { Slot, router } from "expo-router";
-import { Appbar } from "react-native-paper";
-import { StyleSheet } from "react-native";
+import CustomAppBar from "../../components/CustomAppbar";
 
-function handelBack() {
-  router.back();
-}
-const styles = StyleSheet.create({
-  appBar: {
-    margin:0,
-  },
-});
 export default function AuthLayout() {
   return (
     <>
-      <Appbar.Header style={styles.appBar}>
-        <Appbar.BackAction onPress={handelBack} />
-      </Appbar.Header>
+      <CustomAppBar/>
       <Slot />
     </>
   );
---
+++ frontend/MobileHub/components/CustomAppbar.tsx
@@ -0,0 +1,16 @@
+import { router } from "expo-router";
+import { Appbar } from "react-native-paper";
+import style from "../assets/styles";
+
+function handelBack() {
+  router.back();
+}
+export default function CustomAppBar() {
+  return (
+    <>
+      <Appbar.Header style={style.appBar}>
+        <Appbar.BackAction onPress={handelBack} />
+      </Appbar.Header>
+    </>
+  );
+}
